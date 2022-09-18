1b806046a217db01dbd90f24f9f1e5f669b66109
odiseo0 <pedro.esserweb@gmail.com>
2022-09-17 22:54:27 -0400

    🩺 Health to check the database

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from typing import Any
+
 from starlite import Starlite, get
+from sqlalchemy import text
+
+from src.core.db import AsyncScopedSession
 
 
-@get(path="/")
+@get("/")
 def index() -> str:
     """Index function that retuns a "Hello World"."""
     return "Hello World"
 
 
-app = Starlite(route_handlers=[index])
+@get("/health", tags=["Health"])
+async def health_check() -> dict[str, Any]:
+    """Health check handler."""
+    assert (await AsyncScopedSession().execute(text("SELECT 1"))).scalar_one() == 1
+    return {"app": "Shinji"}
+
+
+app = Starlite(route_handlers=[index, health_check])
